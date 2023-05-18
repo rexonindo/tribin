0a02b4dd3659a0fd69693e88b0bf52eaf45f4eaa
anasuryana <anasuryana10@gmail.com>
2023-05-18 22:42:19 +0700

    redirect

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PageController extends Controller
 {
-    function welcome(){
-        return view('welcome');
+    function welcome()
+    {
+        if (Auth::check()) {
+            return redirect('home');
+        } else {
+            return view('welcome');
+        }
     }
 }
