270e75cd4e7a63fd49ede9b6113a3a6b1caa85e3
anasuryana <anasuryana10@gmail.com>
2023-06-05 17:37:37 +0700

    Fix : penyesuaian nama tabel item master

--- app/Models/M_ITM.php
+++ app/Models/M_ITM.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class M_ITM extends Model
 {
     use HasFactory;
-    protected $table = 'm_itm';    
+    protected $table = 'M_ITM';    
     protected $fillable = ['MITM_ITMCD', 'MITM_ITMNM','MITM_ITMTYPE','MITM_STKUOM','MITM_BRAND','MITM_MODEL','MITM_SPEC','MITM_ITMCAT'];
 }
