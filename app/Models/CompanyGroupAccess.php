bea28dd67a77daf3a943984467da8384af924801
anasuryana <anasuryana10@gmail.com>
2023-06-29 09:32:53 +0700

    Fix : tidak bisa login dengan mulus

--- app/Models/CompanyGroupAccess.php
+++ app/Models/CompanyGroupAccess.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyGroupAccess extends Model
 {
     use HasFactory;
+    protected $table = 'COMPANY_GROUP_ACCESSES';
     protected $fillable = [
         'nick_name', 'connection', 'created_by', 'deleted_at', 'deleted_by', 'updated_by'
     ];
