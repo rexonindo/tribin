1a04fd4718cdf72c77527fdb42c0ce765b39ad85
anasuryana <anasuryana10@gmail.com>
2023-06-29 09:34:54 +0700

    Fix : huruf besar kecil

--- app/Models/CompanyGroup.php
+++ app/Models/CompanyGroup.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyGroup extends Model
 {
     use HasFactory;
+    protected $table = 'COMPANY_GROUPS';
     protected $fillable = ['name', 'connection', 'address', 'phone', 'fax'];
 }
