a27e076c7c31ba33e5922c320144176561e7cfaf
Ana Suryana di PSI <ana.suryana@smt.co.id>
2023-08-23 10:11:19 +0700

    Fix : ubah presisi float

--- database/migrations/2023_08_21_144031_add_type_to_t_quohead_table.php
+++ database/migrations/2023_08_21_144031_add_type_to_t_quohead_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('T_QUOHEAD', function (Blueprint $table) {
             $table->string('TQUO_TYPE', 1);
-            $table->float('TQUO_SERVTRANS_COST');
+            $table->float('TQUO_SERVTRANS_COST',12,2);
         });
     }
 
