1bb92f79f48a74b292e2ad8a96ae22f2a3a68773
anasuryana <anasuryana10@gmail.com>
2023-11-12 17:00:42 +0700

    Fix : tambah kolom di tabel kasir

--- app/Models/C_CASHIER.php
+++ app/Models/C_CASHIER.php
@@ -10,7 +10,7 @@ class C_CASHIER extends Model
     use HasFactory;
     protected $table = 'C_CASHIER';
     protected $fillable = [
-        'CCASHIER_BRANCH', 'CCASHIER_ISSUDT', 'CCASHIER_ITMCD', 'CCASHIER_LOCATION', 'CCASHIER_PRICE', 'CCASHIER_REFF_DOC', 'CCASHIER_REMARK',
+        'CCASHIER_BRANCH', 'CCASHIER_ISSUDT', 'CCASHIER_ITMCD', 'CCASHIER_LOCATION', 'CCASHIER_PRICE', 'CCASHIER_REFF_DOC', 'CCASHIER_REMARK', 'CCASHIER_USER',
         'created_by', 'updated_by', 'deleted_at', 'deleted_by'
     ];
 }
