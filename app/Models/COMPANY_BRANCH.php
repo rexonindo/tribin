c06a018a9c0ecc7b4569e3ae8114fe35797461e9
Ana Suryana <anasuryana10@gmail.com>
2023-12-26 20:29:40 +0700

    Fix : penomoran quotation

--- app/Models/COMPANY_BRANCH.php
+++ app/Models/COMPANY_BRANCH.php
@@ -11,6 +11,6 @@ class COMPANY_BRANCH extends Model
     protected $table = 'COMPANY_BRANCHES';
     protected $fillable = [
         'name', 'address', 'connection', 'phone', 'fax', 'invoice_letter_id', 'created_by', 'updated_by',
-        'BRANCH', 'letter_head'
+        'BRANCH', 'letter_head', 'quotation_letter_id'
     ];
 }
